a334d0f97ba9d371efca13036856ece2d39f4e7c
dialite <raymondolisa01@gmail.com>
2022-11-02 22:17:18 +0100

    Implement feedback recommendations

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import './index.css';
 const taskList = document.querySelector('.list');
 const task = [
   {
-    id: 0,
+    index: 0,
     description: 'House chores',
     completed: false,
   },
   {
-    id: 1,
+    index: 1,
     description: 'Read school books',
     completed: false,
   },
   {
-    id: 2,
+    index: 2,
     description: 'Lunch break',
     completed: false,
   },
   {
-    id: 3,
+    index: 3,
     description: 'Play games',
     completed: false,
   },
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
   task.forEach((elem) => {
     const listItem = document.createElement('li');
     listItem.setAttribute('class', 'item');
-    listItem.innerHTML = `<i class="far fa-square" id=${elem.id}></i>
+    listItem.innerHTML = `<i class="far fa-square" id=${elem.index}></i>
     <p class='description'>${elem.description}</p>
     <i class="fas fa-ellipsis-v"></i>`;
     taskList.appendChild(listItem);
